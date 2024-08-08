a12baf36fe1e10dee8df558e5d2e7559cb6ccffa
SofiaP2002 <118442841+SofiaP2002@users.noreply.github.com>
2024-08-08 11:20:08 -0400

    Update InicioSesion.js

--- src/login/InicioSesion.js
+++ src/login/InicioSesion.js
@@ -32,7 +32,7 @@ export const InicioSesion = () => {
     e.preventDefault();
 
     try {
-      const peticion = await fetch("${window.location.origin}/api/iniciarSesion", {
+      const peticion = await fetch("https://deploy-back-six.vercel.app/api/iniciarSesion", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
